2950f0d47d10c54b305b59b1976f40e164af27bc
Julien Collard <juliencollard2001@gmail.com>
2024-03-06 15:58:46 +0100

    Update

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 from abstract_agent import Agent
-from simple_agent import DecisionTreeAgent
+from one_step_agent import OneStepAgent
 from environment import Environment
 from board import Board
 from game_runner import GameRunner
 
 gr = GameRunner()
 
-agent_A = DecisionTreeAgent(nb_future=1)
-agent_B = DecisionTreeAgent(nb_future=2)
+agent_A = OneStepAgent()
+agent_B = Agent()
 
 # gr.run_and_show(agent_A, agent_B, console = True, gif = True)
 
-win_A, win_B, mean_t = gr.compare_agents(agent_A, agent_B, 10)
+win_A, win_B, mean_t = gr.compare_agents(agent_A, agent_B, 30)
 print(f'Agent A won {win_A} times')
 print(f'Agent B won {win_B} times')
 print(f'Mean time of a game: {mean_t}')
